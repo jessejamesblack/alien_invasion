69ab0a85931804a0245cf168b184e6e79257c1dd
jessejamesblack <jessejamesblack@optonline.net>
2017-07-12 20:32:27 -0400

    finished

--- settings.py
+++ settings.py
@@ -1,44 +1,44 @@
 class Settings():
-	# Settings for alien invasion
 
-	def __init__(self):
-		# intialize
-		# screen
-		self.screen_width = 1200
-		self.screen_height = 800
-		self.bg_color = (230, 230, 230)
+    def __init__(self):
+        # Screen 
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        # Ship 
+        self.ship_limit = 3
+            
+        # Bullet 
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+        
+        # Alien 
+        self.fleet_drop_speed = 10
+            
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase.
+        self.score_scale = 1.5
+    
+        self.initialize_dynamic_settings()
 
-		# ship
-		self.ship_limit = 3
-
-		# missiles
-		self.bullet_width = 3
-		self.bullet_height = 15
-		self.bullet_color = 60, 60, 60
-		self.bullets_allowed = 3
-		
-		# alien
-		self.fleet_drop_speed = 10
-
-		self.speedup_scale = 1.1
-		
-		self.score_scale = 1.5
-
-		self.intialize_dynamic_settings()
-
-	def intialize_dynamic_settings(self):
-		self.ship_speed_factor = 1.5
-		self.bullet_speed_factor = 3
-		self.alien_speed_factor = 1
-		self.fleet_direction = 1
-
-		self.alien_points = 50
-
-	def increase_speed(self):
-		self.ship_speed_factor *= self.speedup_scale
-		self.bullet_speed_factor *= self.speedup_scale
-		self.alien_speed_factor *= self.speedup_scale
-		self.fleet_direction *= self.speedup_scale
-
-		self.alien_points = int(self.alien_points * self.score_scale)
-		print(self.alien_points)
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+        
+        # Scoring.
+        self.alien_points = 50
+    
+        # fleet_direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
+        
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        
+        self.alien_points = int(self.alien_points * self.score_scale)
--- ship.py
+++ ship.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
 
 	def __init__(self, ai_settings, screen):
 		# init ship and set pos
